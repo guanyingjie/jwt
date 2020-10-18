9c964abd300557733fc93cd5211ad0f04cd423e1
yingjie.guan <yingjie.guan@yingjieguandeMac-mini.local>
2020-10-18 15:25:45 +0800

    [yingjie][]

---
+++ src/main/java/com/GuavaCache/guava/guavaApplication.java
@@ -0,0 +1,13 @@
+package com.JWT.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class guavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtApplication.class, args);
+    }
+
+}
